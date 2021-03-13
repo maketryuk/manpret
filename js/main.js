585b4b1d9b27f01411de2d8ceade9cb50061863a
Roman <roman.sav15@gmail.com>
2021-03-13 11:21:18 +0200

    mobile version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,56 @@
 let front = {
   hamburger: $('.hamburger'),
-  nav: $('.navbar'),
+  header: $('.header'),
+  headerCategories: $('.header-bottom'),
+  body: $('body'),
   init: function () {
       this.events();      
   },
   toggleNav: function () {
+    let mainNavHeight = $('.header-top').height();
+    console.log(mainNavHeight);
     if (!this.hamburger.hasClass('open')) {
         this.hamburger.addClass('open');
-        this.nav.slideDown('fast');
+        this.header.addClass('active');
+        this.body.addClass('overflow');
+        this.headerCategories.css('padding-top', mainNavHeight + 10);
         } else {
             this.hamburger.removeClass('open');
-            this.nav.hide();
+            this.header.removeClass('active');
+            this.body.removeClass('overflow');
         }
     },
 
 
   events: function () {
       let self = this;
+
+    
+
+          // if (window.matchMedia("(max-width: 992px)").matches) {
+      //         let categories = $('.single-categories--mobile');
+      //         let categoriesH = (-categories.height() - 52 );
+      //         $(categories).css('margin-top', categoriesH);
+      //     } else {
+      //         null
+      //     }
+
+
       $(document).on('click', '.hamburger', function () {
           self.toggleNav();
+      });
+      $(document).on('click', '.close-mobile-menu', function () {
+          self.toggleNav();
+      });
+      $(document).on('click', '.sidebar-mobile-toggler', function () {
+        $('.sidebar').toggleClass('active');
+      });
+      // $(document).on('click', '.close-sidebar', function () {
+      //   $('.sidebar').removeClass('active');
+      // });
+      $(document).on('click', '.products-text .more-btn', function () {
+        $(this).hide();
+        $('.products-text p').css('display', 'block');
       });
 
       $(document).on('click', '.sidebar-title', function () {
